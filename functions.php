dca375dda8e33d78669c15efbcc8c6f63491706b
tobiforsdyke <tobiforsdyke@gmail.com>
2019-10-25 12:22:57 +0100

    Changed all function names from ju_ to tf_

--- functions.php
+++ functions.php
@@ -2,12 +2,14 @@
 
 // Setup
 // REMEMBER TO SET DEV MODE TO FALSE
-define( 'JU_DEV_MODE', true ); //Defines whether in development mode to refresh the cache on stylesheets
+define( 'TF_DEV_MODE', true ); //Defines whether in development mode to refresh the cache on stylesheets
 
 // Includes
 include( get_theme_file_path( '/includes/front/enqueue.php' ) );
+include( get_theme_file_path( '/includes/setup.php' ) );
 
 // Hooks
-add_action( 'wp_enqueue_scripts', 'ju_enqueue' );
+add_action( 'wp_enqueue_scripts', 'tf_enqueue' );
+add_action( 'after_theme_setup', 'tf_setup_theme' );
 
 // Shortcodes
---
+++ includes/setup.php
@@ -0,0 +1,5 @@
+<?php
+
+function tf_setup_theme(){
+  register_nav_menu( 'primary', __('Primary Menu'), 'antagonist' );
+}
